08c5f7f9b48bc3929087bbcab466f90db2d8b885
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-09-19 16:21:11 -0300

    add: Radixsort

---
+++ sort/quicksort.h
@@ -0,0 +1,48 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+int particione(int vetor[], int left, int right)
+{
+   int x = vetor[left];
+   srand(time(NULL));
+   // int x = left + rand() % (right - left + 1);
+   int down = left;
+   int up = right;
+
+   while (down < up)
+   {
+      while (vetor[down] <= x && down < right)
+      {
+         down = down+1;
+      }
+      while (vetor[up] > x)
+      {
+         up = up-1;
+      }
+      if (down < up)
+      {
+         int aux = vetor[down];
+         vetor[down] = vetor[up];
+         vetor[up] = aux;
+      }
+   }
+   vetor[left] = vetor[up];
+   vetor[up] = x;
+   return up;
+}
+
+void quicksortRecurse(int vetor[], int left, int right)
+{
+   int i;
+   if (right > left){
+      i = particione(vetor, left, right);
+      quicksortRecurse(vetor, left, i-1);
+      quicksortRecurse(vetor, i+1, right);
+   }
+}
+
+void quicksort(int vetor[], int tam) {
+   quicksortRecurse(vetor, 0, tam-1);
+}
+
+#endif
