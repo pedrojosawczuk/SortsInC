92e3ae6e57130aea5a3a9d98d3603cb2473fcbef
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2023-09-19 16:21:28 -0300

    add: quicksort

---
+++ sort/radixsort.h
@@ -0,0 +1,43 @@
+#ifndef RADIXSORT_H
+#define RADIXSORT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void radixsort(int vetor[], int tam)
+{
+   int base = 10, r, m, i, j;;
+   int digit = 7;
+   int *B = (int *)malloc(tam * sizeof(int));
+   int *C = (int *)malloc(base * sizeof(int));
+
+   for (m = 0; m < digit; m++)
+   {
+      for (i = 0; i < base; ++i)
+         C[i] = 0;
+
+      for (j = 0; j < tam; ++j)
+      {
+         r = (vetor[j] / (int)(pow(10.0, m))) % base;
+         ++C[r];
+      }
+
+      for (i = 1; i < base; ++i)
+         C[i] += C[i - 1];
+
+      for (j = tam - 1; j >= 0; --j)
+      {
+         r = (vetor[j] / (int)(pow(10.0, m))) % base;
+         i = --C[r];
+         B[i] = vetor[j];
+      }
+
+      for (j = 0; j < tam; ++j)
+         vetor[j] = B[j];
+   }
+
+   free(B);
+   free(C);
+}
+
+#endif
